77601e0184d41ee59c3f2fdf18468fdf398569be
tdot33 <chet.f.chung@gmail.com>
2023-08-03 18:44:45 -0400

    request models

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -1,22 +1,41 @@
 import requests
 import json
-# import related models here
+from .models import CarDealer
 from requests.auth import HTTPBasicAuth
 
 
 # Create a `get_request` to make HTTP GET requests
-# e.g., response = requests.get(url, params=params, headers={'Content-Type': 'application/json'},
-#                                     auth=HTTPBasicAuth('apikey', api_key))
-
+def get_request(url, **kwargs):
+    print(kwargs)
+    print("GET from {} ".format(url))
+    try:
+        response = requests.get(url, params=params, headers={'Content-Type': 'application/json'},
+        auth=HTTPBasicAuth('apikey', api_key))
+    except:
+        print("Network exception occurred")
+    status_code = response.status_code
+    print("With status {}".format(status_code))
+    json_data = json.loads(response.text)
+    return json_data
 
 # Create a `post_request` to make HTTP POST requests
 # e.g., response = requests.post(url, params=kwargs, json=payload)
 
 
 # Create a get_dealers_from_cf method to get dealers from a cloud function
-# def get_dealers_from_cf(url, **kwargs):
-# - Call get_request() with specified arguments
-# - Parse JSON results into a CarDealer object list
+def get_dealers_from_cf(url, **kwargs):
+    results = []
+    json_result = get_request(url)
+    if json_result:
+        dealers = json_result["rows"]
+        for dealer in dealers:
+            dealer_doc = dealer["doc"]
+            dealer_obj = CarDealer(address=dealer_doc["address"], city=dealer_doc["city"], full_name=dealer_doc["full_name"],
+            id=dealer_doc["id"], lat=dealer_doc["lat"], long=dealer_doc["long"],
+            short_name=dealer_doc["short_name"],
+            st=dealer_doc["st"], zip=dealer_doc["zip"])
+            results.append(dealer_obj)
+    return results
 
 
 # Create a get_dealer_reviews_from_cf method to get reviews by dealer id from a cloud function
